f3899994a335bd01e8265def192395a8a504270d
Marco Garcia <marco.silva.garcia2901@gmail.com>
2022-08-27 02:36:34 -0300

    cleaned the data from request

--- notebooks/Users/rm92249@fiap.com.br/prob.py
+++ notebooks/Users/rm92249@fiap.com.br/prob.py
@@ -1,7 +1,6 @@
 # Databricks notebook source
 import requests
 
-
 def useApi(access_token):
     global my_headers
     my_headers = {
@@ -9,9 +8,7 @@ def useApi(access_token):
         'Content-type' : 'application/json'
     }
 
-
-
-useApi("")
+useApi("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYWUxMTEwY2Q1NDIyOGFkZjc3NzFhZmJiNTBmYjBlMCIsInN1YiI6IjYzMDJkZTcwN2Q0MWFhMDA4MTUzYjkxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tRr8KtqXlDujZjIaC5FVpLaHj11O3H4-2EvD48zLjXo")
 
 query = {'query': 'A'}
 response = requests.get("https://api.themoviedb.org/3/search/movie?", headers=my_headers, params=query)
@@ -35,24 +32,62 @@ while i < 100:
     arr.append(movies_list.json())
     i = i + 1
 
+
+
+# COMMAND ----------
+
+for i in arr:
+    print(f"{i}\n")
+len(arr)
+
 # COMMAND ----------
 
 import pandas as pd
 
-output = pd.DataFrame()
+arr = pd.DataFrame(arr)
 
-for i in arr:
-    for key, val in i.items():
-        if key == "success" and val == False:
+#for i in arr:
+    #for key, val in i.items():
+        #if key == "success" and val == False:
             #print("Data not found")
-            break
-        else:
-            output = output.append(i, ignore_index=True)
+            #break
+        #else:
             #print(f"{key} : {val}")
+            #print(i)
+            #output = output.append(i, ignore_index=True)
     #print("----------")
 
-print(output.head())
+#print(arr.iloc[[1]])
+update = arr
+for i, x in enumerate(update['status_code']):
+    if x == 34:
+        update = update.drop(i)
+print(update)
 
 # COMMAND ----------
 
-print(output['genres'])
+pd.reset_option('max_columns')
+print(arr['genres'])
+
+# COMMAND ----------
+
+df = spark.createDataFrame(arr)
+df.show(n=50,vertical=True)
+
+# COMMAND ----------
+
+df2 = df.filter(df.status_code==34)
+df2.show()
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+df.write.format("json").mode("overwrite").save("/teste/dadosFilmes")
+
+# COMMAND ----------
+
+df_success = spark.read.json('dbfs:/teste/dadosFilmes/part-00000-tid-5064522810776596884-efae8b49-69bb-455c-86c5-a84674f7490d-32-1-c000.json')
+df.show(vertical=True)
